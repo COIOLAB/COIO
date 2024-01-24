673c10f18e7da84be4a152010b001a733082b646
SnigdhaPatwatkar <smpatwatkar2000@gmail.com>
2024-01-24 15:00:59 +0530

    deployed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WhatsappComponent } from './whatsapp/whatsapp.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WhatsappComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
